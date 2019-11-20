fecc80427b4bdf42050458f6bc5d0a13e3bfc1e8
darts <darts@tcd.ie>
2019-11-20 14:30:39 +0000

    Mucked about

---
+++ testing/index.js
@@ -0,0 +1,15 @@
+console.log("yo mama");
+let a;
+console.log(a);
+
+let person = {
+    name: "dartse",
+    age: 20
+};
+
+console.log(person);
+console.log(person.age);
+console.log(person['name']);
+
+let lookVal = 'name';
+console.log(person[lookVal]);
